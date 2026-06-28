65279f0c5ce1c906fce719858a6fd25376c45a75
Yuvaraj Kushwaha <yuvrajkushwaha728@gmail.com>
2026-06-28 23:00:21 +0530

    Changes

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from backend.api.interview import router
 
 from backend.database import Base, engine
-import backend.models  # Ensure models are imported so that they are registered with SQLAlchemy
+import backend.models
 
 app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "AI Interview Coach Backend is running 🚀"
+    }
+
 
 app.include_router(router)
 
