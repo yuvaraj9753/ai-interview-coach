7796c7130b6e777d94793e35df9485990067efd7
Yuvaraj Kushwaha <yuvrajkushwaha728@gmail.com>
2026-06-29 14:45:38 +0530

    Changes

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
-DATABASE_URL = "sqlite:///./interview_coach.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
